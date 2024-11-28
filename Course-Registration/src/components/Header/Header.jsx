c21d5989d18ba4d52fc0ef23938d7466329b6f01
kaiem1 <abdurrahmankaiem03@gmail.com>
2024-11-28 19:18:01 +0600

    Implement navbar using Tailwind CSS

---
+++ Course-Registration/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-5xl bg-black text-white font-bold text-center m-5 p-2'>Course Registration</h1>
+        </div>
+    );
+};
+
+export default Header;
